83494d8acc21f7076a53c2797d3f82d227259a59
Enrico <franciscono.enry@gmail.com>
2022-12-22 17:03:38 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+export const config = {
+    "port": 3000,
+    "sourceHost": "192.168.0.1",
+    "sourcePort": 4400
+};
---
+++ index.js
@@ -0,0 +1,42 @@
+import { config } from './config.js';
+
+import http from 'http';
+import express from 'express';
+import { Server } from 'socket.io';
+import dgram from 'dgram';
+
+let app = express();
+let server = http.Server(app);
+let io = new Server(server, {
+  serveClient: true,
+});
+
+app.use('/', express.static('./site'));
+
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => console.log('Client disconnected'));
+    socket.join('video');
+})
+
+var client = dgram.createSocket('udp4');
+
+client.on('listening', function () {
+    var address = client.address();
+    console.log('UDP Client listening on ' + address.address + ":" + address.port);
+    client.setBroadcast(true)
+    client.setMulticastTTL(128); 
+    client.addMembership(config.sourceHost, address.address);
+});
+
+client.on('message', function (message, remote) {   
+    io.to('video').emit('video', message);
+});
+
+client.bind(config.sourcePort, config.sourceHost);
+
+
+
+app.listen(config.port, () => {
+	console.log(`App listening on port ${config.port}`);
+});
